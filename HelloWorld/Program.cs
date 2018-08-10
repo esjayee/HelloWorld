df27dd4dad8e5337e83692d8161ed4bdbf9e9bf2
susan.emmons <susan.emmons@AG007075>
2018-08-10 11:04:32 +0100

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        // starting point of our program
+        static void Main(string[] args)
+        {
+            // implicit conversion
+            int num = 123545646;
+            long bigNum = num;
+
+            float myFloat = 154.23f;
+            double myNewDouble = myFloat;
+
+            double myDouble = 3.14;
+            int myInt;
+
+            bool sunIsShining = true;
+
+            // explicit conversion: cast double to int
+            myInt = (int)myDouble;
+
+            // type conversion
+            string myString = myFloat.ToString();
+
+            Console.WriteLine(myInt);
+            Console.WriteLine(myString);
+            Console.WriteLine($"Sun is shining?: {sunIsShining.ToString()}");
+        }
+
+    }
+}
