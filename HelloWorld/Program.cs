d0492fad2749a3bf6c17ff68d604b8e24f168c12
susan.emmons <susan.emmons@AG007075>
2018-08-10 14:39:47 +0100

    Methods 1

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,53 +8,23 @@ namespace HelloWorld
 {
     class Program
     {
-        // starting point of our program
         static void Main(string[] args)
         {
-            // challenge
-            byte biteMe = 4;
-            sbyte mySbyte = -23;
-            int myInt = -56;
-            uint myUInt = 45;
-            short myShort = -34;
-            ushort myUShort = 45 * 3;
-            long myLong = 3567;
-            ulong myULong = -45 + 50;
-            float myFloat = 45.35f;
-            double myDouble = 43645757.12456;
-            char myChar = '£';
-            bool systemReady = true;
-            decimal myDecimal = 3.14756m;
-            string myString1 = "I control text";
-            string myString2 = "38";
+            WriteSomething();
+            WriteSomethingSpecific("I am an argument and am called from a method");
 
-            int num1 = Int32.Parse(myString2);
-
-            Console.WriteLine(biteMe);
-            Console.WriteLine(mySbyte);
-            Console.WriteLine(myInt);
-            Console.WriteLine(myUInt);
-            Console.WriteLine(myShort);
-            Console.WriteLine(myUShort);
-            Console.WriteLine(myLong);
-            Console.WriteLine(myULong);
-            Console.WriteLine(myFloat);
-            Console.WriteLine(myDouble);
-            Console.WriteLine(myChar);
-            Console.WriteLine(systemReady);
-            Console.WriteLine(myDecimal);
-            Console.WriteLine(myString1);
-            Console.WriteLine(myString2);
-            Console.WriteLine(num1);
-
-
-
-
-
-
-
-
+            Console.Read();
         }
 
+        // access modifier (static) return type method type(parameter1, parameter2)
+        public static void WriteSomething()
+        {
+            Console.WriteLine("I am called from a method");
+        }
+
+        public static void WriteSomethingSpecific(string myText)
+        {
+            Console.WriteLine(myText);
+        }
     }
 }
